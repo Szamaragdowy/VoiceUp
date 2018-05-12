e8a5c12583a6a945454adc7dbdc271ede3f7e82e
Szamaragdowy <springi775@gmail.com>
2018-05-12 16:27:35 +0200

    Seperation

---
+++ Client/VoiceUP/Structures/SerwerConnectionInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceUP.Structures
+{
+    class SerwerConnectionInfo
+    {
+        public IPEndPoint IPAdres { get;}
+
+        public string Name { get; set; }
+
+        SerwerConnectionInfo(IPEndPoint ipAdres,String name)
+        {
+            this.IPAdres = ipAdres;
+            this.Name = name;
+        }
+    }
+}
