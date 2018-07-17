4efdf47312af6cfb4e52fdc91581011129d01ec1
Sławomir Asimowicz <bekaslayer@gmail.com>
2018-07-17 15:31:55 +0200

    komunikacja klient serwer + multicast

---
+++ Client/VoiceUP/Connections.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace VoiceUP
+{
+    class Connections
+    {
+        private Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private const int bufSize = 8 * 1024;
+        private State state = new State();
+        private EndPoint epFrom = new IPEndPoint(IPAddress.Any, 0);
+        private AsyncCallback recv = null;
+        UdpClient client = new UdpClient();
+        private string multicastAddr = "239.0.0.222";
+
+        public class State
+        {
+            public byte[] buffer = new byte[bufSize];
+        }
+
+        public void Server(string address, int port)
+        {
+            _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, true);
+            _socket.Bind(new IPEndPoint(IPAddress.Parse(address), port));
+            Receive();
+        }
+
+        public void Client(string address, int port)
+        {
+            _socket.Connect(IPAddress.Parse(address), port);
+            //Receive();
+            ReceiveData();
+        }
+
+        public void Send(string text)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            _socket.BeginSend(data, 0, data.Length, SocketFlags.None, (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndSend(ar);
+                Console.WriteLine("SEND: {0}, {1}", bytes, text);
+            }, state);
+        }
+        public void Send(byte[] data)
+        {
+            _socket.BeginSend(data, 0, data.Length, SocketFlags.None, (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndSend(ar);
+                //Console.WriteLine("SEND: {0}, {1}", bytes, text);
+            }, state);
+        }
+        //test
+        public void SendLoop(string addr, int port)
+        {
+            byte[] bytes;
+            string q = "";
+            Connections c = new Connections();
+            c.Client(addr, port);
+            while (q != "Quit")
+            {
+                q = Console.ReadLine();
+                bytes = Encoding.ASCII.GetBytes(q);
+                c.Send(bytes);
+            }
+        }
+        //test
+        public void ReceiveData()
+        {
+            client.ExclusiveAddressUse = false;
+            IPEndPoint localEp = new IPEndPoint(IPAddress.Any, 2222);
+
+            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            client.ExclusiveAddressUse = false;
+
+            client.Client.Bind(localEp);
+
+            IPAddress multicastaddress = IPAddress.Parse(multicastAddr);
+            client.JoinMulticastGroup(multicastaddress);
+
+            //Console.WriteLine("Listening this will never quit so you will need to ctrl-c it");
+
+            while (true)
+            {
+                Byte[] data = client.Receive(ref localEp);
+                string strData = Encoding.ASCII.GetString(data);
+                Console.WriteLine(strData);
+            }
+        }
+        private void Receive()
+        {
+            _socket.BeginReceiveFrom(state.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv = (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndReceiveFrom(ar, ref epFrom);
+                _socket.BeginReceiveFrom(so.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv, so);
+                /*
+                 * funkcja z naudio łapiąca dźwięk itp so.buffer jest w postaci (byte[])
+                 */
+                //Console.WriteLine("RECV: {0}: {1}, {2}", epFrom.ToString(), bytes, Encoding.ASCII.GetString(so.buffer, 0, bytes));
+            }, state);
+        }
+
+    }
+}
---
+++ Server/VoiceUpServer/Connections.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace VoiceUpServer
+{
+    class Connections
+    {
+        private Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private const int bufSize = 8 * 1024;
+        private State state = new State();
+        private static int multicastPort = 8080;
+        private EndPoint epFrom = new IPEndPoint(IPAddress.Any, 0);
+        private AsyncCallback recv = null;
+        UdpClient udpclient = new UdpClient();
+        UdpClient multicast = new UdpClient();
+        IPEndPoint multicastIPEP = new IPEndPoint(IPAddress.Parse("239.0.0.222"), multicastPort);
+
+        public class State
+        {
+            public byte[] buffer = new byte[bufSize];
+        }
+
+        public void Server(string address, int port)
+        {
+            _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, true);
+            _socket.Bind(new IPEndPoint(IPAddress.Parse(address), port));
+            Receive();
+        }
+
+        public void Client(string address, int port)
+        {
+            _socket.Connect(IPAddress.Parse(address), port);
+            Receive();
+        }
+
+        public void Send(string text)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            _socket.BeginSend(data, 0, data.Length, SocketFlags.None, (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndSend(ar);
+                Console.WriteLine("SEND: {0}, {1}", bytes, text);
+            }, state);
+        }
+        //funkcje dodawania i usuwania multicasta nie wiem jak rozbić multicasta :/
+        /*
+        public void AddToMulticastGroup(string addr)
+        {
+            IPAddress ip = IPAddress.Parse(addr);
+            multicast.JoinMulticastGroup(ip);
+        }
+        public void DropFromMulticastGroup(string addr)
+        {
+            IPAddress ip = IPAddress.Parse(addr);
+            multicast.DropMulticastGroup(ip);
+        }
+        */
+        public void SendMulticast(string kek)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(kek);
+            multicast.Send(bytes, bytes.Length, multicastIPEP);
+            multicast.Close();
+        }
+        public void SendMulticast(byte[] kek)
+        {
+            multicast.Send(kek, kek.Length, multicastIPEP);
+            multicast.Close();
+        }
+        //Receive and Send to another clients
+        private void Receive()
+        {
+            _socket.BeginReceiveFrom(state.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv = (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndReceiveFrom(ar, ref epFrom);
+                _socket.BeginReceiveFrom(so.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv, so);
+                //Send to all users in multicast
+                SendMulticast(so.buffer);
+                //checking data
+                //Console.WriteLine("RECV: {0}: {1}, {2}", epFrom.ToString(), bytes, Encoding.ASCII.GetString(so.buffer, 0, bytes));
+            }, state);
+        }
+    }
+}
