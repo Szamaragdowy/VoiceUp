ed6602c6aaf0a93ca05fef368a65b1ba50952655
Sławomir Asimowicz <bekaslayer@gmail.com>
2018-07-21 19:09:59 +0200

    protokół ale trzeba sporo podczepić

---
+++ Client/VoiceUP/Protocol.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceUP
+{
+    class Protocol
+    {
+        //format komunikatu LOGIN/param1/param2/param3
+        public string komunikat="";
+        public string FirstLogToServer()
+        {
+            string[] datas = komunikat.Split('/');
+            switch (datas[0])
+            {
+                case "":
+                    return "JOIN";
+                case "SEND_P":
+                    //wpisywanie danych zaszyfrowanych otrzymanym kluczem publicznym w datas[1]
+                    return "LOGIN"; // dodatkowo dopisać po "/" login haslo suma kontrolna
+            }
+            return CommunicationProtocol();
+        }
+        public string CommunicationProtocol()
+        {
+            string[] kek = komunikat.Split('/');
+            switch (kek[0])
+            {
+                case "LOGIN_ACK":
+                    break;
+                case "LOGIN_NAK":
+                    //Okienko message box o tym że nie można ustanowić połączenia z powodu błędnych parametrów
+                    break;
+                case "CHECK":
+                    return "CHECK_Y";
+                case "AKT_USR":
+                    //W pozostałych parametrach są użytkownicy na serwerze więc wyświetlanie ich będzie pokazane w gridzie
+                    break;
+                case "FULL":
+                    //Okienko message box o tym że serwer jest pełny i nie można ustanowić połączenia oraz powracamy do grida w którym łączymy się z serwerem
+                    break;
+                default:
+                    break;
+            }
+            return "Something goes wrong";//okienko messege box
+        }
+        public string CloseComunication()
+        {
+            //doputy serwer nie odpowie nam cya nie zamykamy aplikacji
+            if (komunikat == "CYA") return "";//wykonaj zakończenie komunikacji/zamknięcie aplikacji/opuściłeś server albo server został wyłączony.
+            else return "CYA";
+        }
+    }
+}
---
+++ Server/VoiceUpServer/Protocol.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceUpServer
+{
+    class Protocol
+    {
+        //format komunikatu LOGIN/param1/param2/param3
+        public string komunikat;
+        //zmienna pomocnicza temp
+        public bool temp = false;
+        //zmienna sprawdzająca czy użytkownik jest połączony
+        public bool loged = false;
+        //funkcja pomocnicza by zobrazować czy parametry są poprawne czy nie
+        public bool Parameters()
+        {
+            if (temp) return true;
+            else return false;
+        }
+        
+        public string ComunicationProtocol()
+        {
+            string[] datas = komunikat.Split('/');
+            if (datas[0] == "CYA")
+            {
+                loged = false;
+                return "CYA";
+            }
+            if (loged == true) return "AKT_USR/";//+ string z użytkownikami po /
+            switch (datas[0])
+            {
+                case "JOIN":
+                    //wyslanie klucza publicznego
+                    return "SEND_P/" + "";//zamiast tego po + jakiś klucz publiczny wygenerowany
+                case "LOGIN":
+                    if (Parameters())
+                    {
+                        loged = true;
+                        //sprawdzenie czy serwer nie jest pełny jesli tak
+                        //return "FULL";
+                        //jeśli nie
+                        return "LOGIN_ACK";
+                    }
+                    else return "LOGIN_NAK";
+                case "CHECK_Y":
+                    return "AKT_USR/";//+ string z użytkownikami po /
+            }
+            return "Something goes wrong";//okienko messege box
+        }
+        public string CheckingActivity()
+        {
+            //co dwie minuty - nie mam pojęcia jak zrobić timer
+            if (loged) return "CHECK";
+            else
+            {
+                return "";
+            }
+        }
+        public string CloseServer()
+        {
+            //do wszystkich podłączonych po TCP wysłać CYA - po otrzymaniu wszystkie powinny otrzymać komunikat że zakończono połączenie  
+            return "CYA";
+        }
+        //trzeba by było zrobić w serwerze klasę użytkownik która by miała w sobie klase własną tego protokołu bo inaczej chyba to nie przejdzie bo każdy powinien łączyć się osobno
+    }
+}
